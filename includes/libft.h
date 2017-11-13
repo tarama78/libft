449f63606633397ae9e0ecc28bc3467d82dced4e
tim <tnicolas@student.42.fr>
2017-11-13 19:21:56 +0100

    Update libft.h

--- includes/libft.h
+++ includes/libft.h
@@ -76,4 +76,6 @@ void		ft_lstadd(t_list **alst, t_list *new);
 void		ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 
+void	*ft_realloc(void *ptr, size_t original_size, size_t new_size);
+
 #endif
